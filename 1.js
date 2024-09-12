1d92c5f553ec449927aba9d2863be825d31ce0c0
Gasana22 <114236235+Gasana22@users.noreply.github.com>
2024-09-12 17:38:21 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,26 @@
+//this is a js app
+
+console.log("Hello World");
+console.log("A93585");
+console.log("MARK");
+
+//innitialization
+var my_name = "MARK"
+
+//declare a varible
+var height;
+
+//assign a variable
+height = 3,4;
+console.log(height);
+console.log(my_name,".",height);
+
+//let
+let area = 90.5;
+let f_name;
+f_name = "MARK"
+console.log(area);
+console.log(f_name);
+
+//const
+const pie = 3.14
